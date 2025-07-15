4f3a5d1b64c3965d8247b10aaf98960fd8c0f969
manuel_cano <manuelcanodw@gmail.com>
2025-07-15 11:03:59 +0200

    Archivo de índice para exportar componentes

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Card } from './card';
+export { default as CardImage } from './CardImage';
+export { default as CardContent } from './CardContent';
+export { default as CardButtons } from './CardButtons';
+export { default as Button } from './Button';
+export { default as Indicator } from './Indicator'; 
